015d4d45ecd49f8abd79a1c7fe19cf90d73c00f3
ahmadzqq <ahmadzqq.@gmail.com>
2020-07-26 22:26:07 +0800

    setup google sign in  having difficulties with sha1 key

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterShare',
+      title: 'Social',
       debugShowCheckedModeBanner: false,
-      home: Text("Hello World"),
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+        accentColor: Colors.teal,
+      ),
+      home: Home(),
     );
   }
 }
